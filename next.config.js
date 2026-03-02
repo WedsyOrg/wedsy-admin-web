8f9d20f96dde2d9f220da38ed551d77bfc86f42b
abhit-onelot <abhit@onelot.ph>
2026-03-02 14:39:14 +0530

    revert: undo cloudflare commits b404687 and 610a619

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
     domains: [
       "wedsy.s3.amazonaws.com",
       "wedsy.s3.ap-south-1.amazonaws.com",
@@ -13,4 +12,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
