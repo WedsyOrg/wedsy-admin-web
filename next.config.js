610a6190ce391bc220f81d92bb9bc3302bd60ec6
abhit-onelot <abhit@onelot.ph>
2026-03-02 14:03:59 +0530

    cloudflare

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: [
       "wedsy.s3.amazonaws.com",
       "wedsy.s3.ap-south-1.amazonaws.com",
